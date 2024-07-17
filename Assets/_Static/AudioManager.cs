a33b010fb332fc5f9b83f9aaaff43af557e0071d
Roger Salas <rogersalas2000@gmail.com>
2024-07-18 01:12:37 +0200

    Audio manager + 2 audio sources

---
+++ Assets/_Static/AudioManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AudioManager
+{
+    public enum SFX
+    {
+        NONE,
+        ATTACK,
+        DESTROY,
+        SELECTCARD,
+        BUTTON
+    }
+
+    public enum Music
+    {
+        NONE,
+        BACKGROUND,
+        WIN
+    }
+
+    public static void SetSFX(SFX sound)
+    {
+        AudioSource sfxAS = GameObject.Find("SFX_Source").GetComponent<AudioSource>();
+
+        sfxAS.Stop();
+
+        string audioName = null;
+
+        switch (sound) 
+        {
+            case SFX.NONE:
+                audioName = "";
+                break;
+            case SFX.ATTACK:
+                audioName = "";
+                break;
+            case SFX.DESTROY:
+                audioName = "";
+                break;
+            case SFX.SELECTCARD:
+                audioName = "";
+                break;
+            case SFX.BUTTON:
+                audioName = "";
+                break;
+        }
+
+        sfxAS.clip = Resources.Load<AudioClip>("Audio/SFX" + audioName);
+        sfxAS.Play();
+    }
+
+    public static void SetMusic(Music sound)
+    {
+        AudioSource musicAS = GameObject.Find("Music_Source").GetComponent<AudioSource>();
+
+        musicAS.Stop();
+
+        string audioName = null;
+
+        switch (sound)
+        {
+            case Music.NONE:
+                audioName = "";
+                break;
+            case Music.BACKGROUND:
+                audioName = "medieval-fantasy-142837";
+                break;
+            case Music.WIN:
+                audioName = "kingdom-of-fantasy-version-60s-10817";
+                break;
+        }
+
+        musicAS.clip = Resources.Load<AudioClip>("Audio/Music" + audioName);
+        musicAS.Play();
+    }
+}
