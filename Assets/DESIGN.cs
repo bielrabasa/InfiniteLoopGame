d202564b259993f44fc709cb7231557a9289bca7
Mohammaddhm <32272531+Mohammaddhm@users.noreply.github.com>
2024-07-21 12:27:12 +0200

    Cards_Updated

--- Assets/DESIGN.cs
+++ Assets/DESIGN.cs
@@ -40,7 +40,7 @@ namespace DESIGN
         public static readonly Color cardHighlightColor = new Color(0.98f, 1.0f, 0.80f, 1.0f);
 
         //---------------SIZES--------------
-        public const float OnBoardCardSize = 3.8f;
+        public const float OnBoardCardSize = 4.5f;
         public const float CardSelectingDistanceToCamera = 3f;
         public const float CardZoomDistanceToCamera = 8f;
 
